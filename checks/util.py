e24096931ad50b453af58b4ad2636ecf92233489
Rico <maomao75979@gmail.com>
2024-01-08 15:32:40 +0800

    fix: remove empty option

--- checks/util.py
+++ checks/util.py
@@ -3,6 +3,9 @@ from typing import List
 def none_str(s: str) -> str:
     return "" if s is None else s
 
+def not_none(s: str) -> bool:
+    return (s is not None) and (s != "")
+
 def process_single_engine_cfg(option: str) -> str:
     if option.startswith("--"):
         return option[2:]
@@ -13,5 +16,8 @@ def process_single_engine_cfg(option: str) -> str:
 def process_engine_cfg(cfg: str) -> str:
     if cfg is None:
         return ""
-    options: List[str] = list(map(process_single_engine_cfg, cfg.split(" ")))
+    options: List[str] = list(
+        map(process_single_engine_cfg,
+            filter(not_none, cfg.split(" ")))
+    )
     return "_".join(options)
